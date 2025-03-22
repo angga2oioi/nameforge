50095168de44661a2e9c899b013868d2322b1d82
Rizka Sanggar Darmawan <angga2oioi@gmail.com>
2025-03-22 16:18:22 +0700

    add default

--- index.js
+++ index.js
@@ -10,36 +10,38 @@ const start = async (seed, position, maxLength, maxWords, tlds) => {
     }
 
     if (!position) {
-        position = await jaci.select("Where should the seed word be placed? :", {
+        position = await jaci.select("Where should the seed word be placed? (default random) :", {
             options: {
                 "start": "S",
                 "middle": "M",
                 "end": "E",
                 "random": "R"
-            }
+            },
+            default: "R"
         })
     }
 
     if (!maxLength) {
-        maxLength = await jaci.number("Enter max length : ")
+        maxLength = await jaci.number("Enter max length (default 8): ", { default: 8 })
     }
 
     if (!maxWords) {
-        maxWords = await jaci.number("Enter max words : ")
+        maxWords = await jaci.number("Enter max words (default 20): ", { default: 20 })
     }
 
     if (!tlds) {
-        tlds = await jaci.string("Enter tlds (separated by spaces) : ")
+        tlds = await jaci.string("Enter tlds (separated by spaces, default com) : ", { default: "com" })
     }
 
     await generateNames(seed?.split(" ")?.sort((a, b) => a.length - b.length), position, maxLength, maxWords, tlds?.split(" "))
 
-    const retry = await jaci.select("Do you want to generate more names? : ", {
+    const retry = await jaci.select("Do you want to generate more names? (default exit) : ", {
         options: {
             "Retry different settings": "R",
             "Retry same settings": "S",
             "Exit": "E"
-        }
+        },
+        default: "E"
     })
 
     if (retry === "Retry different settings") {
